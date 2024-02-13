2805ee4270cd5b39bcba7d66a31ef146e2056181
Juan Carlos Salazar Silva <salazarsilvajc@gmail.com>
2024-02-13 00:48:55 -0800

    Cambios de imagen en page principal

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -13,9 +13,6 @@ import {
 import CardWrapper from '@/app/ui/dashboard/cards';
 
 export default async function Page() {
-  
-
-
   return (
     <main>
       <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
