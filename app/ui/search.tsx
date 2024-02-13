fe02cd85b3d950ae52c0278d093fd047527d6a01
Juan Carlos Salazar Silva <salazarsilvajc@gmail.com>
2024-02-12 23:00:12 -0800

    Se agregaron las actions create, update y delete

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -16,6 +16,7 @@ const handleSearch = useDebouncedCallback((term) => {
   
 
     const params = new URLSearchParams(searchParams);
+    params.set('page', '1');
     if (term) {
       params.set('query', term);
     } else {
